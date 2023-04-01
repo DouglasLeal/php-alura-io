68cb9197dfebfd386d38dcd7081f663e9fc2b8c2
Douglas Leal <douglas.leal@live.com>
2023-04-01 09:14:53 -0300

    criando filtro

--- 05_filtro.php
+++ 05_filtro.php
@@ -1,6 +1,11 @@
 <?php
-$arquivo = fopen('lista-cursos.txt', 'r');
 
-stream_filter_append($arquivo, 'string.toupper');
+require 'MeuFiltro.php';
+
+$arquivo = fopen('lista-cursos.txt', 'r');
+stream_filter_register('alura.partes', MeuFiltro::class);
+
+//stream_filter_append($arquivo, 'string.toupper');
+stream_filter_append($arquivo, 'alura.partes');
 
 echo fread($arquivo, filesize('lista-cursos.txt'));
